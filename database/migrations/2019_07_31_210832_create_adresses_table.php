df7d40400311a93f00e47f9daf4e56608bbd018c
Maks <ffwmaksshcherbyna@gmail.com>
2019-08-01 03:42:48 +0300

    added address model and migration

---
+++ database/migrations/2019_07_31_210832_create_adresses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->string('city', 255);
+            $table->string('area', 255);
+            $table->string('street', 255);
+            $table->string('house_number', 255);
+            $table->text('additional_information');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adresses');
+    }
+}
