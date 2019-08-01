ea1e1b196b86ad589dbbd1bd1ea71049991f4cc1
Maks <ffwmaksshcherbyna@gmail.com>
2019-08-01 03:49:37 +0300

    added AdressController and routes

---
+++ app/Http/Controllers/AdressController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use App\Adress;
+
+class AdressController extends Controller
+{
+    private $settingRules = [
+        'name'    => 'required',
+        'area' => 'required',
+        'city'    => 'required',
+    ];
+
+    /**
+     * Get all adresse from DB
+     * @return string
+    */
+    public function index()
+    {
+        $adresses = Adress::all();
+
+        if (!count($adresses)) {
+            return response()->json(['success'=>false], 202);
+        }
+
+        return response()->json(['success'=>true, 'data'=>$adresses], 200);
+    }
+
+    /**
+     * Add new adress to DB
+     * @param Request $request
+     * @return string
+    */
+    public function create(Request $request)
+    {
+        $requestData = $request->all();
+        $validator   = Validator::make($requestData, $this->settingRules);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response()->json(['success'=>false, 'data'=>$errors->all()], 202);
+        }
+
+        $insertData = [
+            'name' =>$requestData['name'],
+            'street' => $requestData['street'] ? $requestData['street'] : '',
+            'house_number' => $requestData['house_number'] ? $requestData['house_number'] : '',
+            'additional_information' => $requestData['additional_information'] ? $requestData['additional_information'] : '',
+            'city' => $requestData['city'],
+            'area' => $requestData['area'],
+        ];
+
+        try {
+            Adress::create($insertData);
+        } catch (Exception $e) {
+            return response()->json(['success'=>false, 'data'=>$e->getMessage()], 500);
+        }
+
+        return response()->json(['success'=>true], 200);
+    }
+
+    /**
+     * Add new adress to DB
+     * @param int $id
+     * @return string
+    */
+    public function remove(int $id)
+    {
+        $adress = Adress::find($id);
+
+        try {
+            if($adress){
+                $adress->delete();
+            }
+        } catch (Exception $e) {
+            return response()->json(['success'=>false, 'data'=>$e->getMessage()], 500);
+        }
+
+        return response()->json(['success'=>true], 200);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('add', 'AdressController@create');
+Route::get('show', 'AdressController@index');
+Route::delete('remove/{projectId}', 'AdressController@remove');
